3ae94e87b407d1d7dec45aeaa60b4b0c5768bda7
lcapps <lcapps5600@gmail.com>
2019-11-26 13:54:52 -0500

    finished

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+
 // Load env
 dotenv.config({ path: './config.env' });
 
@@ -14,14 +15,14 @@ if (process.env.NODE_ENV === 'development') {
 // Profile routes
 app.use('/api/v1/profile', require('./routes/profile'));
 
-// // Handle production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static(__dirname + '/public/'));
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(__dirname + '/public/'));
 
-//   // Handle SPA
-//   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-// }
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 8000;
 
